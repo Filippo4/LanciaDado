9e8ff37ba22a3ee795a450cf3ffc3e631d0fc440
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-04 13:18:26 +0100

    Aggiunta la puntata e imagini del dado

--- LanciaDado/MainWindow.xaml.cs
+++ LanciaDado/MainWindow.xaml.cs
@@ -29,25 +29,45 @@ namespace LanciaDado
         {
             int numero = int.Parse(TxtNumero.Text);            
             Random random = new Random();
-            int numeroCasuale = random.Next(1,6);
-            if (numero==numeroCasuale)
+            int numeroCasuale = random.Next(1,7);
+            int puntata = int.Parse(Txt_Puntata.Text);
+            int credito = int.Parse(Lbl_credito.Content.ToString());
+            if (puntata <= credito)
             {
-                TxtRisultato.Text = numeroCasuale + " Complimenti hai vinto";
-            }
-            else
+                if (numero == numeroCasuale)
+                {
+                    TxtRisultato.Text = numeroCasuale + " Complimenti hai vinto";
+                    credito = credito + (puntata * 4);
+                    Lbl_credito.Content = credito;
+                }
+                else
+                {
+                    TxtRisultato.Text = numeroCasuale + " Mi dispiace hai perso";
+                    credito = credito - puntata;
+                    Lbl_credito.Content = credito;
+                }
+                Uri resourceUri = new Uri($"/Images/dado{numeroCasuale}.png", UriKind.Relative);
+                Img_Dado.Source = new BitmapImage(resourceUri);
+                if (numero < 1 || numero > 6)
+                {
+                    MessageBox.Show("il numero non è valido", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Hand);
+                }
+                if(credito == 0)
+                {
+                    MessageBox.Show("Game Over non hai più soldi", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Hand);
+                }
+            }else
             {
-                TxtRisultato.Text = numeroCasuale + " Mi dispiace hai perso";
-            }
-            if (numero < 1 || numero > 6)
-            {
-                MessageBox.Show("il numero non è valido","Attenzione",MessageBoxButton.OK,MessageBoxImage.Hand);
+                MessageBox.Show("Non puoi puntare quello che non hai", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Hand);
             }
         }
 
         private void BtnReset_Click(object sender, RoutedEventArgs e)
         {
             TxtNumero.Clear();
-            TxtNumero.Clear();
+            TxtRisultato.Clear();
+            Lbl_credito.Content = 100;
+            Txt_Puntata.Clear();
         }
     }
 }
